007f02c8da599c0841670fbcb90637322cd0e7da
anasofiperezz <123486693+anasofiperezz@users.noreply.github.com>
2024-05-09 12:18:41 -0600

    Update app.py

--- app.py
+++ app.py
@@ -15,28 +15,33 @@ def welcome():
 @app.route('/orders', methods=['GET'])
 def get_orders():
     all_orders = db.list_orders()
-    result = [{'id': order['id'], 'cantidad': order['cantidad']} for order in all_orders]
+    result = []
+    for order in all_orders:
+        order_info = {
+            'id': order.id,
+            'id_usuario': order.id_usuario,
+            'timestamp': order.timestamp,
+            'productos': []
+        }
+        for product in order.products:
+            product_info = {
+                'id_producto': product.product_id,
+                'cantidad': product.quantity
+            }
+            order_info['productos'].append(product_info)
+        result.append(order_info)
     return jsonify(result), 200
 
 @app.route('/orders', methods=['POST'])
 def create_order():
     data = request.json
-    new_order_data = {
-        'id_usuario': data['id_usuario'],
-        'timestamp': data['timestamp'],
-        'id_producto': data['id_producto'],
-        'id_pedido': data['id_pedido'],
-        'cantidad': data['cantidad']
-    }
-    order_id = db.add_order(new_order_data)    
+    new_order = db.add_order(data['id_usuario'])
 
     for product_data in data['productos']:
-        product_data['order_id'] = order_id
-
         db.add_order_products(
-            order_id=product_data['order_id'],
-            product_id=product_data['product_id'],
-            quantity=product_data['quantity']
+            order_id=new_order.id,
+            product_id=product_data['id_producto'],
+            quantity=product_data['cantidad']
         )
 
     return jsonify({'message': 'Pedido creado correctamente'}), 201
@@ -45,7 +50,19 @@ def create_order():
 def get_order(order_id):
     order = db.get_order(order_id)
     if order:
-        return jsonify({'id': order['id'], 'cantidad': order['cantidad']}), 200
+        order_info = {
+            'id': order.id,
+            'id_usuario': order.id_usuario,
+            'timestamp': order.timestamp,
+            'productos': []
+        }
+        for product in order.products:
+            product_info = {
+                'id_producto': product.product_id,
+                'cantidad': product.quantity
+            }
+            order_info['productos'].append(product_info)
+        return jsonify(order_info), 200
     else:
         return jsonify({'message': 'Pedido no encontrado'}), 404
 
